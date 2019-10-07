83096fc05f02cbcc00791705e1bb4e715b0f92ba
czen0002 <czen0002@student.monash.edu>
2019-10-08 01:22:23 +1100

    first

---
+++ app.js
@@ -0,0 +1,53 @@
+//https://hub.packtpub.com/building-movie-api-express/
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const actors = require('./routers/actor');
+const movies = require('./routers/movie');
+
+const app = express();
+
+app.listen(8080);
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", express.static(path.join(__dirname, "dist/movieAng")));
+
+mongoose.connect('mongodb://localhost:27017/movies', { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+    if (err) {
+        return console.log('Mongoose - connection error:', err);
+    }
+    console.log('Connect Successfully');
+
+});
+
+//Configuring Endpoints
+//Actor RESTFul endpoionts 
+app.get('/actors', actors.getAll);
+app.post('/actors', actors.createOne);
+app.get('/actors/:id', actors.getOne);
+app.put('/actors/:id', actors.updateOne);
+app.post('/actors/:id/movies', actors.addMovie);
+//task2
+app.delete('/actors/:id', actors.deleteOne);
+//task3
+app.put('/actors/:id/:movieid', actors.removeMovie);
+
+
+//Movie RESTFul  endpoints
+app.put('/movies/:year1', movies.deleteMovies);
+app.get('/movies', movies.getAll);
+app.post('/movies', movies.createOne);
+app.get('/movies/:id', movies.getOne);
+app.put('/movies/:id', movies.updateOne);
+//task1
+app.delete('/movies/:id', movies.deleteOne);
+//task4
+app.put('/movies/:id/:actorid', movies.removeActor);
+//task5
+app.post('/movies/:id/actors', movies.addActor);
+//task6
+app.get('/movies/:year1/:year2', movies.getMoviesPeriod);
---
+++ src/app/actor/actor.component.ts
@@ -0,0 +1,121 @@
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from "../database.service";
+
+@Component({
+  selector: 'app-actor',
+  templateUrl: './actor.component.html',
+  styleUrls: ['./actor.component.css']
+})
+export class ActorComponent implements OnInit {
+
+  constructor(private dbService:DatabaseService) { }
+
+  ngOnInit() {
+    this.onGetActors();
+    this.onGetMovies();
+  }
+
+  actorsDB: any[] = [];
+  moviesDB: any[] = [];
+  section = 1;
+  fullName: string = "";
+  bYear: number = 0;
+  actorId: string = "";
+  title: string = "";
+  year: number = 0;
+  movieId: string = "";
+
+  // Get all Actors
+  onGetActors() {
+    this.dbService.getActors().subscribe((data: any[]) => {
+      this.actorsDB = data;
+    });
+  }
+
+  // Create a new Actor, POST request
+  onSaveActor() {
+    let obj = { name: this.fullName, bYear: this.bYear };
+    this.dbService.createActor(obj).subscribe(result => {
+      this.onGetActors();
+    });
+  }
+
+  // Update an Actor
+  onSelectUpdate(item) {
+    this.fullName = item.name;
+    this.bYear = item.bYear;
+    this.actorId = item._id;
+  }
+
+  onUpdateActor() {
+    let obj = { name: this.fullName, bYear: this.bYear };
+    this.dbService.updateActor(this.actorId, obj).subscribe(result => {
+      this.onGetActors();
+    });
+  }
+
+  //Delete Actor
+  onDeleteActor(item) {
+    this.dbService.deleteActor(item._id).subscribe(result => {
+      this.onGetActors();
+    });
+  }
+
+  // Get all Movies
+  onGetMovies() {
+    this.dbService.getMovies().subscribe((data: any[]) => {
+      this.moviesDB = data;
+    });
+  }
+
+  // Create a new Movie, POST request
+  onSaveMovie() {
+    let obj = {title: this.title, year: this.year};
+    this.dbService.createMovie(obj).subscribe(result => {
+      this.onGetMovies();
+    });
+  }
+
+  onSelectMovie(item){
+    this.title = item.title;
+    this.year = item.year;
+    this.movieId = item._id;
+  }
+
+  // Update Movie
+  onUpdataMovie(){
+    let id = this.movieId;
+    let obj = { name: this.fullName, bYear: this.bYear, id: this.actorId };
+    this.dbService.updateMoive(id, obj).subscribe(result => {
+      this.onGetMovies();
+    })
+  }
+
+  // Delete Movie
+  onDeleteMovie(item) {
+    this.dbService.deleteMovie(item._id).subscribe(result => {
+      this.onGetMovies();
+    })
+  }
+
+  onDeleteMovies() {
+    let year = this.year;
+    this.dbService.deleteMovies(year).subscribe(result => {
+      this.onGetMovies();
+    })
+  }
+
+  resetValues() {
+    this.fullName = "";
+    this.bYear = 0;
+    this.actorId = "";
+    this.title = "";
+    this.year = 0;
+    this.movieId = "";
+  }
+
+  changeSection(sectionId) {
+    this.section = sectionId;
+    this.resetValues();
+  }
+}
